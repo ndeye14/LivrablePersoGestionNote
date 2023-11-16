393a213f1fbf5ead0b4d6ee5b97b8ba97b46b25a
Germainita <dioufgermainedaba@gamil.com>
2023-11-16 12:12:53 +0000

    Route pour admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { DashbordAdminComponent } from './admin/dashbord-admin/dashbord-admin.co
 import { AccueilProfComponent } from './prof/accueil-prof/accueil-prof.component';
 import { ListeNotesComponent } from './apprenant/liste-notes/liste-notes.component';
 import { AuthComponent } from './auth/auth.component';
+import { GestionProfComponent } from './admin/gestion-prof/gestion-prof.component';
+import { GestionApprenantComponent } from './admin/gestion-apprenant/gestion-apprenant.component';
 
 const routes: Routes = [
   // On définit la route par défaut 
@@ -14,6 +16,12 @@ const routes: Routes = [
 
   // On définit les routes pour l'administrateur
   {path: 'admin', component: DashbordAdminComponent},
+
+  // Pour gestion des professeurs
+  {path: 'admin-gestionProf', component: GestionProfComponent},
+
+  // Pour gestion des apprenants
+  {path: 'admin-gestionApprenant', component: GestionApprenantComponent},
 
   // Route pour interface professeur
   {path: 'prof', component:AccueilProfComponent},
